7d1e5b2b07e7d1962a471a3e8fb6943e78dba90e
aal-brown <adlai.brown@gmail.com>
2020-02-08 23:04:29 +0000

    Added more resources seeds for test users 1 and 4.

--- db/seeds/02_resources_seeds.sql
+++ db/seeds/02_resources_seeds.sql
@@ -1,9 +1,21 @@
 INSERT INTO resources (user_id, title, description, resource_url, thumbnail_url, date)
-VALUES (1, 'wikipedia jQuery', 'Wikipedia article on jQuery', 'https://en.wikipedia.org/wiki/JQuery','https://en.wikipedia.org/wiki/JQuery#/media/File:JQuery_logo.svg', '2020-01-01T00:00:00.000Z'),
+VALUES
 (1, 'wikipedia jQuery', 'Wikipedia article on jQuery', 'https://en.wikipedia.org/wiki/JQuery','https://en.wikipedia.org/wiki/JQuery#/media/File:JQuery_logo.svg', '2020-01-01T00:00:00.000Z'),
 
+(1, 'wikipedia AJAX', 'Really good reference on AJAX, I found it helpful for understanding the meaning of life.', 'https://en.wikipedia.org/wiki/Ajax_(programming)','../../public/images/no-thumb.png', '2020-01-01T00:01:01.000Z'),
 
+(1, 'MDN Array Methods Reference', 'Great reference for array methods on MDN.', 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array','../../public/images/no-thumb.png', '2020-01-01T00:02:02.000Z'),
 
+(1, 'Eloquent JavaScript', 'Fantastic free book on JavScript, available to read in your browser. It deals with JS as a craft, and is about writing good or eloquent code.', 'https://eloquentjavascript.net/','https://eloquentjavascript.net/img/cover.jpg', '2020-01-01T00:03:03.000Z'),
 
+(1, 'Python Tutor - Visualize Code Execution', 'This is a good resource for familiarizing yourself with how programs run in different languages.', 'http://pythontutor.com/visualize.html#mode=edit','../../public/images/no-thumb.png', '2020-01-01T00:04:05.000Z'),
 
-'../../public/images/no-thumb.png'
+(4, 'Hyperphysics', 'This is an amazing website where you can learn practically any topic in physics, while being able to view the concepts in a well thought-out web of connected ideas that are used to navigate the topics. It helps connect the different concepts in a visual way that makes it much easier to understand. Each page is also full of visuals and fairly light on text. The language is easily readable as compared to the high level of language used in many Wikipedia pages for the same topic.', 'http://hyperphysics.phy-astr.gsu.edu/hbase/hframe.html','http://hyperphysics.phy-astr.gsu.edu/hbase/images2/Hphconw.gif', '2020-01-01T00:00:00.000Z'),
+
+(4, 'YouTube Neural Network Architectures Short Lecture', 'This lecture describes the wide variety of neural network architectures available to solve various problems.', 'https://www.youtube.com/watch?v=oJNHXPs0XDk','../../public/images/no-thumb.png', '2020-01-01T00:01:01.000Z'),
+
+(4, 'YouTube Video on Irregular French Verbs In The Present Tense', 'This lecture covers irregular present tense verbs in an easy to understand way.', 'https://www.youtube.com/watch?v=1Nv-gKU0t2U','../../public/images/no-thumb.png', '2020-01-01T00:02:02.000Z'),
+
+(4, 'YouTube Video on How To Say This, That, These, and Those in French.', 'Title is self-explanatory, is a really well presented introduction to these very common words.', 'https://www.youtube.com/watch?v=4q_s_8MWHfY','../../public/images/no-thumb.png', '2020-01-01T00:03:03.000Z'),
+
+(4, 'Introductory YouTube video on learning to speak Icelandic', 'Pretty good, there arent many resources for learning Icelandic so I thought Id put this up.', 'https://www.youtube.com/watch?v=jF81VMAtn6w','../../public/images/no-thumb.png', '2020-01-01T00:04:00.000Z');
