21ece56e215f76899f30c9e4f617f3c06bc23a9e
NoahDumba <noahdumba1@gmail.com>
2020-02-08 23:25:03 +0000

    fix syntax error in resource seeds

--- db/seeds/02_resources_seeds.sql
+++ db/seeds/02_resources_seeds.sql
@@ -11,16 +11,16 @@ VALUES
 
 
 (2, 'wikipedia jQuery', 'Wikipedia article on jQuery (this resource was stolen and reposted from another user ;P)', 'https://en.wikipedia.org/wiki/JQuery','https://en.wikipedia.org/wiki/JQuery#/media/File:JQuery_logo.svg', '2020-01-01T10:00:00.000Z'),
-(2, 'Differential Calc', 'Differential Calculus excercises and lessons on Khan Academy', 'https://www.khanacademy.org/math/differential-calculus','https://cdn.kastatic.org/images/khan-logo-dark-background.new.png', '2020-01-01T15:00:00.000Z')
-(2, 'Code Challenges', 'Challenging coding exercises', 'https://code-exercises.com/programming/hard','../../public/images/no-thumb.png', '2020-01-02T00:00:00.000Z')
-(2, 'History of Philosophy', 'Comprehensive collection of Philosophy; Ancient and Modern, Eastern and Western', 'https://www.friesian.com/history.htm','https://www.friesian.com/images/Athens.jpg', '2020-01-02T05:00:00.000Z')
-(2, 'Automation (Im Scared)', 'Interesting Kurzgesagt video on the topic of ever increasing automation', 'https://www.youtube.com/watch?v=WSKi8HfcxEk','https://i.ytimg.com/vi/WSKi8HfcxEk/maxresdefault.jpg', '2020-01-02T07:00:00.000Z')
+(2, 'Differential Calc', 'Differential Calculus excercises and lessons on Khan Academy', 'https://www.khanacademy.org/math/differential-calculus','https://cdn.kastatic.org/images/khan-logo-dark-background.new.png', '2020-01-01T15:00:00.000Z'),
+(2, 'Code Challenges', 'Challenging coding exercises', 'https://code-exercises.com/programming/hard','../../public/images/no-thumb.png', '2020-01-02T00:00:00.000Z'),
+(2, 'History of Philosophy', 'Comprehensive collection of Philosophy; Ancient and Modern, Eastern and Western', 'https://www.friesian.com/history.htm','https://www.friesian.com/images/Athens.jpg', '2020-01-02T05:00:00.000Z'),
+(2, 'Automation (Im Scared)', 'Interesting Kurzgesagt video on the topic of ever increasing automation', 'https://www.youtube.com/watch?v=WSKi8HfcxEk','https://i.ytimg.com/vi/WSKi8HfcxEk/maxresdefault.jpg', '2020-01-02T07:00:00.000Z'),
 
-(3, 'SQL Injection Attack', 'Computerphile video explaining SQL injection attacks', 'https://www.youtube.com/watch?v=ciNHn38EyRc','https://i.ytimg.com/vi/ciNHn38EyRc/maxresdefault.jpg', '2020-01-01T05:00:00.000Z')
-(3, 'WWII', 'Wikipedia page for WWII', 'https://en.wikipedia.org/wiki/World_War_II','../../public/images/no-thumb.png', '2020-01-01T10:00:00.000Z')
-(3, 'Canadian History', 'History of Canada Wikipedia page', 'https://en.wikipedia.org/wiki/History_of_Canada','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1200px-Flag_of_Canada_%28Pantone%29.svg.png', '2020-01-01T15:00:00.000Z')
-(3, 'Camera Basics', 'Basics of what camera settings do and how to use them', 'https://streetbounty.com/basic-camera-settings/','https://cdn.mos.cms.futurecdn.net/MivVp3YuKbuvWZnRrBDQWV.jpg', '2020-01-02T03:00:00.000Z')
-(3, 'Learn Python!', 'Learn to code in of the most powerful and frustrating programming languages around!', 'https://www.learnpython.org/','https://www.andreabacciu.com/wp-content/uploads/2015/02/Python-Logo-PNG-Image.png', '2020-01-02T08:00:00.000Z')
+(3, 'SQL Injection Attack', 'Computerphile video explaining SQL injection attacks', 'https://www.youtube.com/watch?v=ciNHn38EyRc','https://i.ytimg.com/vi/ciNHn38EyRc/maxresdefault.jpg', '2020-01-01T05:00:00.000Z'),
+(3, 'WWII', 'Wikipedia page for WWII', 'https://en.wikipedia.org/wiki/World_War_II','../../public/images/no-thumb.png', '2020-01-01T10:00:00.000Z'),
+(3, 'Canadian History', 'History of Canada Wikipedia page', 'https://en.wikipedia.org/wiki/History_of_Canada','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1200px-Flag_of_Canada_%28Pantone%29.svg.png', '2020-01-01T15:00:00.000Z'),
+(3, 'Camera Basics', 'Basics of what camera settings do and how to use them', 'https://streetbounty.com/basic-camera-settings/','https://cdn.mos.cms.futurecdn.net/MivVp3YuKbuvWZnRrBDQWV.jpg', '2020-01-02T03:00:00.000Z'),
+(3, 'Learn Python!', 'Learn to code in of the most powerful and frustrating programming languages around!', 'https://www.learnpython.org/','https://www.andreabacciu.com/wp-content/uploads/2015/02/Python-Logo-PNG-Image.png', '2020-01-02T08:00:00.000Z'),
 
 (4, 'Hyperphysics', 'This is an amazing website where you can learn practically any topic in physics, while being able to view the concepts in a well thought-out web of connected ideas that are used to navigate the topics. It helps connect the different concepts in a visual way that makes it much easier to understand. Each page is also full of visuals and fairly light on text. The language is easily readable as compared to the high level of language used in many Wikipedia pages for the same topic.', 'http://hyperphysics.phy-astr.gsu.edu/hbase/hframe.html','http://hyperphysics.phy-astr.gsu.edu/hbase/images2/Hphconw.gif', '2020-01-01T00:00:00.000Z'),
 
