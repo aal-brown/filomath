56f82df91ea61dc43def0f27c6864a90efb0d213
NoahDumba <noahdumba1@gmail.com>
2020-02-09 00:14:49 +0000

    add comment seeds

---
+++ db/seeds/05_comments_seeds.sql
@@ -0,0 +1,64 @@
+INSERT INTO resources (resource_id, user_id, message, date)
+VALUES
+(1, 2, 'Neat article', '2020-01-01T00:00:00.000Z'),
+(1, 3, 'WOW jQuery is so cool!', '2020-01-01T01:00:00.000Z'),
+(1, 1, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(2, 4, 'Neat article', '2020-01-01T00:00:00.000Z'),
+(2, 3, 'WOW ajax is so cool!', '2020-01-01T01:00:00.000Z'),
+(2, 1, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(3, 2, 'Neat article', '2020-01-01T00:00:00.000Z'),
+(3, 3, 'WOW array methods are so cool!', '2020-01-01T01:00:00.000Z'),
+(3, 1, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(4, 2, 'Neat book', '2020-01-01T00:00:00.000Z'),
+(4, 3, 'WOW JS is so cool!', '2020-01-01T01:00:00.000Z'),
+(4, 1, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(5, 2, 'Neat visualization', '2020-01-01T00:00:00.000Z'),
+(5, 3, 'WOW programming is so cool!', '2020-01-01T01:00:00.000Z'),
+(5, 1, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(6, 3, 'WOW programming is so cool!', '2020-01-01T01:00:00.000Z'),
+(6, 2, 'ikr', '2020-01-01T01:00:00.000Z'),
+(6, 1, 'this is the exact same as my resource!', '2020-01-01T02:00:00.000Z'),
+
+(7, 4, 'Neat excercises', '2020-01-01T00:00:00.000Z'),
+(7, 3, 'WOW calculus is so cool!', '2020-01-01T01:00:00.000Z'),
+(7, 2, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(8, 1, 'Neat challenges', '2020-01-01T00:00:00.000Z'),
+(8, 3, 'WOW challenges are so cool!', '2020-01-01T01:00:00.000Z'),
+(8, 2, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(9, 1, 'Neat website', '2020-01-01T00:00:00.000Z'),
+(9, 3, 'WOW philosophy is so cool!', '2020-01-01T01:00:00.000Z'),
+(9, 2, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(10, 1, 'Neat video', '2020-01-01T00:00:00.000Z'),
+(10, 3, 'WOW automation is so crazy!', '2020-01-01T01:00:00.000Z'),
+(10, 2, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(11, 1, 'Neat video', '2020-01-01T00:00:00.000Z'),
+(11, 4, 'WOW injection attacks are so crazy!', '2020-01-01T01:00:00.000Z'),
+(11, 3, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(12, 1, 'Neat article', '2020-01-01T00:00:00.000Z'),
+(12, 4, 'YIKES this war was so crazy!', '2020-01-01T01:00:00.000Z'),
+(12, 3, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(14, 1, 'Neat article', '2020-01-01T00:00:00.000Z'),
+(14, 4, 'I luv caemra!', '2020-01-01T01:00:00.000Z'),
+(14, 3, 'Thanks guys, dont forget to rate the resource!', '2020-01-01T02:00:00.000Z'),
+
+(15, 3, 'Y nobody comment?', '2020-01-01T02:00:00.000Z'),
+
+(16, 3, 'Y nobody comment?', '2020-01-01T02:00:00.000Z'),
+
+(17, 4, 'nueral network make my brain hurt', '2020-01-01T02:00:00.000Z'),
+
+(18, 2, 'what language is this video?', '2020-01-01T02:00:00.000Z'),
+
+(20, 3, 'ive heard theres like 3 people in iceland', '2020-01-01T02:00:00.000Z'),
+(20, 3, 'one of them is my cousin Oskar!', '2020-01-01T02:00:00.000Z');
