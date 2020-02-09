30513b4ae0060d37c577481e23705daf19368518
aal-brown <adlai.brown@gmail.com>
2020-02-09 00:54:03 +0000

    Created a file to make loading the seeds faster.

---
+++ db/load_seeds.sql
@@ -0,0 +1,9 @@
+/*
+\i ./db/seeds/01_users_seeds.sql
+\i ./db/seeds/02_resources_seeds.sql
+*/
+\i ./db/seeds/03_ratings_seeds.sql
+\i ./db/seeds/04_likes_seeds.sql
+\i ./db/seeds/05_comments_seeds.sql
+\i ./db/seeds/06_categories.sql
+\i ./db/seeds/07_resource_categories.sql
